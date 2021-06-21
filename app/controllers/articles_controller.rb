b272e636dabd86d89e4665ae878717c6b735dc15
Akash KUmar <kumar.ryan94@gmail.com>
2021-06-21 17:35:13 +0530

    crearion of form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,5 +8,15 @@ class ArticlesController < ApplicationController
     @articles = Article.all
 
   end
+  def new
+   
+
+  end 
+
+  def create
+    render plain: params[:article]
+   
+
+  end
 
 end
